819b0abc9a2316a14911254ac8b204df72ff88eb
Landon Spell <spell.landon@gmail.com>
2022-03-22 10:27:24 -0500

    Add bit button with some styling

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from '@zoxllc/base-ui.button';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button
+        label='Hello Sebi'
+        className='border-2 px-2 py-1 text-center text-blue-900'
+      />
+    </div>
+  );
+}
+
+export default App;
